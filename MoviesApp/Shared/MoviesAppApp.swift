0f7246afca3763c1856bd177ea7b41f14e09e9d4
Aashish <aashish1aug@gmail.com>
2022-02-18 12:52:12 +0530

    Create Movies App

---
+++ MoviesApp/Shared/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  Shared
+//
+//  Created by Aashish Tyagi on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
